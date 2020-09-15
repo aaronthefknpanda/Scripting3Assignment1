a0ca2b8d00efa7c8e9f617f94a067b2b823d9278
aaronthefknpanda <64624240+aaronthefknpanda@users.noreply.github.com>
2020-09-15 18:06:14 -0400

    DOTween Test
    Using differenct DOTween functions in Unity.

---
+++ Scripting 3 Assignment 1/Assets/Script/DotweenTest.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using System.Linq.Expressions;
+
+public class DotweenTest : MonoBehaviour
+{
+    public bool movingRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        movingRight = true;
+        transform.DOMove(new Vector3(4.0f, 4.0f, 0.0f), 1);
+        transform.DORotate(new Vector3(0.0f, 0.0f, -180.0f), 0.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Wall")
+        {
+            if(movingRight)
+            {
+                movingRight = false;
+                transform.DOMove(new Vector3(-4.0f, 4.0f, 0.0f), 1);
+                transform.DORotate(new Vector3(0.0f, 0.0f, 0.0f), 0.5f);
+            }
+            else if(!movingRight)
+            {
+                movingRight = true;
+                transform.DOMove(new Vector3(4.0f, 4.0f, 0.0f), 1);
+                transform.DORotate(new Vector3(0.0f, 0.0f, -180.0f), 0.5f);
+            }
+
+            transform.DOPunchScale(new Vector3(0.5f, 0.5f, 0.5f), 0.5f);
+        }
+    }
+}
---
+++ Scripting 3 Assignment 1/Assets/Script/DotweenWallTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class DotweenWallTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Cube")
+        {
+            transform.DOPunchPosition(new Vector3(0.0f, 0.0f, 0.5f), 1);
+        }
+    }
+}
